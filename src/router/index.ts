3d56d87a0f19a53aff0014893e517fa1d8b4d400
mberkayarsln <mberkayarslann@gmail.com>
2026-03-07 16:21:12 +0300

    add route guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "@/views/HomeView.vue"
 import SignInView from "@/views/SignInView.vue";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,8 @@ const router = createRouter({
         {
             path: "/",
             name: "home",
-            component: HomeView
+            component: HomeView,
+            meta: { requiresAuth: true }
         },
         {
             path: "/sign-in",
@@ -16,6 +18,35 @@ const router = createRouter({
             component: SignInView
         }
     ]
+})
+
+const getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+        const removeListener = onAuthStateChanged(
+            getAuth(),
+            (user) => {
+                removeListener();
+                resolve(user);
+            },
+            reject
+        );
+    })
+}
+
+router.beforeEach(async (to, from, next) => {
+    const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+    if (requiresAuth) {
+        const user = await getCurrentUser();
+        if (user) {
+            next();
+        } else {
+            next("/sign-in");
+        }
+    }
+    else {
+        next();
+    }
 })
 
 export default router;
